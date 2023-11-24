4e46c2e852ec0f0ffc6527569ef883844f148a9d
Omnia <omniaeid266@gmail.com>
2023-11-24 15:15:23 -0600

    jenkensApp is committed

---
+++ jenkensApp/src/main/java/com/example/jenkensApp/JenkensAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkensApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkensAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkensAppApplication.class, args);
+	}
+
+}
---
+++ jenkensApp/src/test/java/com/example/jenkensApp/JenkensAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkensApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkensAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
